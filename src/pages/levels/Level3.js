e8dbf3618ab87cc6dc3a418dda12a6c40f4c81c8
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-05-07 21:23:23 +0530

    -level 3

--- src/pages/levels/Level3.js
+++ src/pages/levels/Level3.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Navbar from '../../components/Navbar'
+import InfoBot from '../../components/InfoBot'
+
+const symbolKey = {
+    '🔥': 'A',
+    '🌊': 'B',
+    '🌲': 'C',
+    '🌞': 'D',
+    '🌸': 'E',
+    '🌴': 'F',
+    '🍁': 'G',
+    '🍎': 'H',
+    '🍇': 'I',
+    '🍋': 'J',
+};
+
+const messageSymbols = ['🌊', '🌞', '🍁', '🌴', '🍎', '🍇', '🔥', '🌲', '🌸', '🍋'];
+
+const correctMessage = 'BEDFICHGJA';
 
 const Level3 = () => {
+    const [inputMessage, setInputMessage] = useState('');
+
+    const onInputChange = (event) => {
+        setInputMessage(event.target.value.toUpperCase());
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (inputMessage === correctMessage) {
+            alert('Correct message!');
+        } else {
+            alert('Incorrect message, please try again');
+        }
+    };
+
     return (
-        <div>Level3</div>
+        <div className='level2-main'>
+            <div className='gear-main'>
+            <h1>Level 3: Communication</h1>
+            <p>Decode the following message by matching symbols to English letters:</p>
+            <p>🌊🌸🌞🌴🍇🌲🍎🍁🍋🔥</p>
+            <ul>
+                {messageSymbols.map((symbol) => (
+                    <li key={symbol}>
+                        {symbol} - {symbolKey[symbol]}
+                    </li>
+                ))}
+            </ul>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="inputMessage">Enter the decoded message:</label>
+                <input type="text" id="inputMessage" value={inputMessage} onChange={onInputChange} />
+                <button type="submit">Submit</button>
+            </form>
+            </div>
+            <Navbar />
+            <InfoBot
+                title={"Level 3: Language Decoding"}
+                desc={"You discover an ancient scroll that is written in a lost language. Using your knowledge of symbols and your attention to detail, you must match each symbol to an English letter to decode the message. Once you've decoded the message, you can use it to solve a puzzle that will provide a clue to the location of the next level."}
+            />
+
+            
+        </div>
     )
 }
 
