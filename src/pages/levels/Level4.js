19524f6ac879d9ffc088b758ac81a88db0a8881b
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-05-07 21:23:58 +0530

    -level 4

--- src/pages/levels/Level4.js
+++ src/pages/levels/Level4.js
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, { useState } from "react"
+import Navbar from '../../components/Navbar'
+import InfoBot from '../../components/InfoBot'
 
 const Level4 = () => {
+    const [step, setStep] = useState(0);
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+    const [input3, setInput3] = useState("");
+    const [input4, setInput4] = useState("");
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handleInput1 = (e) => {
+        setInput1(e.target.value);
+        if (e.target.value.toLowerCase() === "needle" || e.target.value.toLowerCase() === "a needle") {
+            setStep(step + 1);
+        }
+    };
+
+    const handleInput2 = (e) => {
+        setInput2(e.target.value);
+        if (e.target.value.toLowerCase() === "bottle" || e.target.value.toLowerCase() === "a bottle") {
+            setStep(step + 1);
+        }
+    };
+
+    const handleInput3 = (e) => {
+        setInput3(e.target.value);
+        if (e.target.value.toLowerCase() === "envelope" || e.target.value.toLowerCase() === "an envelope") {
+            setStep(step + 1);
+        }
+    };
+
+    const handleInput4 = (e) => {
+        setInput4(e.target.value);
+        if (e.target.value.toLowerCase() === "future" || e.target.value.toLowerCase() === "the future") {
+            setShowMessage(true);
+        }
+    };
+
     return (
-        <div>Level4</div>
+        <div className="level2-main">
+            <div className="gear-main">
+            <h1>Challenging Puzzle</h1>
+            {step === 0 && (
+                <div className="question">
+                    <p>Question 1: What has a head and a tail, but no body?</p>
+                    <input type="text" value={input1} onChange={handleInput1} className = "level4-input" placeholder="Enter your answer..."/>
+                </div>
+            )}
+            {step === 1 && (
+                <div className="question">
+                    <p>Question 2: What has a neck but no head, and wears a cap?</p>
+                    <input type="text" value={input2} onChange={handleInput2} className = "level4-input" placeholder="Enter your answer..."/>
+                </div>
+            )}
+            {step === 2 && (
+                <div className="question">
+                    <p>What starts with an "E", ends with an "E", but only contains one letter?</p>
+                    <input type="text" value={input3} onChange={handleInput3} className = "level4-input" placeholder="Enter your answer..."/>
+                </div>
+            )}
+            {step === 3 && (
+                <div className="question">
+                    <p>What is always in front of you but can't be seen?</p>
+                    <input type="text" value={input4} onChange={handleInput4} className = "level4-input" placeholder="Enter your answer..."/>
+                </div>
+            )}
+            {showMessage && (
+                <p style={{ color: "green" }}>
+                    Congratulations! You have solved the challenging puzzle!
+                </p>
+            )}
+            </div>
+            <InfoBot
+                title={"Level 4: Object Recognition"}
+                desc={"You find yourself in a dark room filled with various objects. Your task is to identify a specific object based on a series of clues and hints provided. The challenge is to use your perseverance and problem-solving skills to determine the correct object and progress to the next level."}
+            />
+
+           
+        </div>
     )
 }
 
